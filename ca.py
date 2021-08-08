59331c1c48f55764ae2b71299469d156e94e9b2b
Timsbim <github@thomas-jacobi.de>
2021-08-08 21:48:29 +0200

    Update

--- ca.py
+++ ca.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 
 def build_rule(n):
-    """Build the rule with number no as a mapping (dictionary):
+    """Build the nth rule with as a mapping (dictionary):
              triple(0/1, 0/1, 0/1) -> new state of cell
                      ^    ^    ^
     state of:      left  cell right
@@ -23,16 +23,16 @@ def print_states(states, on="*", off=" "):
     print("\n".join("".join(on if s else off for s in row) for row in states))
 
 
-def evolve(rule_n, start_state, iterations, folder=None):
-    """Calculate the evolution of a row of cellular automata given in
-    start_state under the rule with number rule over iterations many steps
-    and saves the result as an image
+def evolve(rule_n, start, iterations, folder=None):
+    """Calculate the evolution of a row of cellular automata given in start
+     under the rule with number rule_n over iterations many steps and saves
+     the result as an image
     """
 
     # Setup: Matrix for recording the states. First row is the given start
-    # state
-    states = np.zeros((iterations + 1, len(start_state)), np.uint8)
-    states[0] = start_state
+    # state.
+    states = np.zeros((iterations + 1, len(start)), np.uint8)
+    states[0] = start
 
     # Build the rule
     rule = build_rule(rule_n)
@@ -55,41 +55,46 @@ def evolve(rule_n, start_state, iterations, folder=None):
 
     return states
 
+
 def evolve_all(
-    *, rules=range(1, 257), start_state=None, iterations=100, folder=None
+    *, rules=range(1, 257), start=None, iterations=100, folder=None
 ):
-    """Plot the evolution with given start state for all rules numbers given
+    """Plot the evolution with given start state for all rule numbers given
     (default = all)
     """
+    if __name__ == "__main__":  # Only for starting the Pool-processes
 
-    # Create folder before multiprocessing to avoid (very unlikely) collision
-    if folder is None:
-        folder = Path.cwd() / "plots"
-    path = Path(folder)
-    if not path.exists():
-        path.mkdir(exist_ok=True)
+        # Create folder if it doesn't exist
+        if folder is None:
+            folder = Path.cwd() / "plots"
+        path = Path(folder)
+        if not path.exists():
+            path.mkdir(exist_ok=True)
+
+        # If no start state is given, use one with all zeroes except for the
+        # cell in the middle
+        if start is None:
+            start = np.zeros(iterations, dtype=np.uint8)
+            m, r = divmod(iterations, 2)
+            start[m + r] = 1
+
+        # Use multiprocessing Pool to speed up the calculations (which are
+        # independent)
+        args = ((n, start.copy(), iterations, path) for n in rules)
+        with Pool(12) as p:
+            p.starmap(evolve, args, chunksize=4)
+
+
+def evolve_animated(rule_n, start=None, iterations=100, folder=None):
 
     # If no start state is given, use one with all zeroes except for the cell
     # in the middle
-    if start_state is None:
-        start_state = np.zeros(iterations, dtype=np.uint8)
-        start_state[iterations // 2] = 1
+    if start is None:
+        start = np.zeros(iterations, dtype=np.uint8)
+        m, r = divmod(iterations, 2)
+        start[m + r] = 1
 
-    # Use a pool to speed up the calculations
-    args = ((n, start_state.copy(), iterations, path) for n in rules)
-    with Pool(12) as p:
-        p.starmap(evolve, args, chunksize=4)
-
-
-def evolve_animated(rule_n, start_state=None, iterations=100, folder=None):
-
-    # If no start state is given, use one with all zeroes except for the cell
-    # in the middle
-    if start_state is None:
-        start_state = np.zeros(iterations, dtype=np.uint8)
-        start_state[iterations // 2] = 1
-
-    states = evolve(rule_n, start_state, iterations * 5, folder=None)
+    states = evolve(rule_n, start, iterations * 5, folder=None)
 
     fig = plt.figure(figsize=(10, 10))
     ax = plt.axes()
@@ -99,13 +104,9 @@ def evolve_animated(rule_n, start_state=None, iterations=100, folder=None):
         ax.clear()
         ax.axis("off")
         if i <= iterations + 1:
-            frame = np.zeros((iterations + 1, len(start_state)), dtype=np.int8)
-            frame[0 : i, :] = states[0:i, :]
+            frame = np.zeros((iterations + 1, len(start)), dtype=np.int8)
+            frame[0 : i, :] = states[0 : i, :]
         else:
-            frame = states[i - iterations : i, :]
+            frame = states[i - iterations : i + 1, :]
         pass
 
-
-if __name__ == "__main__":
-
-    evolve_all()
