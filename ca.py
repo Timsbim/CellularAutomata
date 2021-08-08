7e9d81d5f1d26485bcd12ccaebad6855e305419e
Timsbim <github@thomas-jacobi.de>
2021-08-08 12:06:02 +0200

    Update

--- ca.py
+++ ca.py
@@ -8,10 +8,10 @@ import matplotlib.pyplot as plt
 
 def build_rule(n):
     """Build the rule with number no as a mapping (dictionary):
-                 triple(0/1, 0/1, 0/1) -> new state of cell
-                         ^    ^    ^
-        state of:      left  cell right
-                     neighbour  neighbour
+             triple(0/1, 0/1, 0/1) -> new state of cell
+                     ^    ^    ^
+    state of:      left  cell right
+                 neighbour  neighbour
     """
     combos = product((0, 1), repeat=3)
     rule_str = f"{bin(n)[-1:1:-1]:0<8}"
@@ -20,7 +20,7 @@ def build_rule(n):
 
 def print_states(states, on="*", off=" "):
     """Print state evolution on console (on for state 1, off for state 0)"""
-    print('\n'.join(''.join(on if s else off for s in row) for row in states))
+    print("\n".join("".join(on if s else off for s in row) for row in states))
 
 
 def evolve(rule_n, start_state, iterations, folder=None):
@@ -41,7 +41,7 @@ def evolve(rule_n, start_state, iterations, folder=None):
     m = states.shape[1] - 1
     for t in range(1, states.shape[0]):
         states[t, 1:m] = [
-            rule[tuple(states[t-1, i-1:i+2])] for i in range(1, m)
+            rule[tuple(states[t - 1, i - 1 : i + 2])] for i in range(1, m)
         ]
 
     # Save result
@@ -50,11 +50,17 @@ def evolve(rule_n, start_state, iterations, folder=None):
         if not path.exists():
             path.mkdir(exist_ok=True)
         plt.imsave(
-            path / f"ca_{rule_n:0>3}.png", states, cmap=plt.get_cmap('Blues')
+            path / f"ca_{rule_n:0>3}.png", states, cmap=plt.get_cmap("Blues")
         )
 
 
-def evolve_all(*, start_state=None, iterations=100, folder=None):
+def evolve_all(
+    *, rules=range(1, 257), start_state=None, iterations=100, folder=None
+):
+    """Plot the evolution with given start state for all rules numbers given
+    (default = all)
+    """
+
     # Create folder before multiprocessing to avoid (very unlikely) collision
     if folder is None:
         folder = Path.cwd() / "plots"
@@ -69,14 +75,11 @@ def evolve_all(*, start_state=None, iterations=100, folder=None):
         start_state[iterations // 2] = 1
 
     # Use a pool for the calculations
-    args = (
-        (n, start_state.copy(), iterations, path) for n in range(1, 257)
-    )
-    with Pool(14) as p:
+    args = ((n, start_state.copy(), iterations, path) for n in rules)
+    with Pool(12) as p:
         p.starmap(evolve, args, chunksize=4)
 
 
 if __name__ == "__main__":
 
     evolve_all()
-
